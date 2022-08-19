52940c4a6a2568f446fe1f1ee731ea2a6f67c153
Parsa <Mehdipourparsa@gmail.com>
2022-08-19 12:51:12 +0430

    refactor : WorkWithDirectories

--- WorkingWithFileSystems/Program.cs
+++ WorkingWithFileSystems/Program.cs
@@ -6,6 +6,8 @@ using static System.Environment;
 OutputFileSystemInfo();
 Console.WriteLine("-----------------------------------");
 WorkWithDrives();
+Console.WriteLine("-----------------------------------");
+WorkWithDirectories();
 
 static void OutputFileSystemInfo()
 {
@@ -50,4 +52,26 @@ static void WorkWithDrives()
             WriteLine("{0,-30} | {1,-10}", drive.Name, drive.DriveType);
         }
     }
+}
+
+static void WorkWithDirectories()
+{
+    // define a directory path for a new folder
+    // starting in the user's folder
+    string newFolder = Combine(
+    GetFolderPath(SpecialFolder.Personal),
+    "Code", "Chapter09", "NewFolder");
+    WriteLine($"Working with: {newFolder}");
+    // check if it exists
+    WriteLine($"Does it exist? {Exists(newFolder)}");
+    // create directory
+    WriteLine("Creating it...");
+    CreateDirectory(newFolder);
+    WriteLine($"Does it exist? {Exists(newFolder)}");
+    Write("Confirm the directory exists, and then press ENTER: ");
+    ReadLine();
+    // delete directory
+    WriteLine("Deleting it...");
+    Delete(newFolder, recursive: true);
+    WriteLine($"Does it exist? {Exists(newFolder)}");
 }
